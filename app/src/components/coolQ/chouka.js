0b38af4023d29122def611a4a20493218c8f97e4
ANWY200211 <88234795+ANWY200211@users.noreply.github.com>
2021-08-01 18:17:23 +0800

    Initialize qqtools2-chouka

---
+++ app/src/components/coolQ/chouka.js
@@ -0,0 +1,176 @@
+/* 抽卡算法 */
+import { templateReplace } from '../../function';
+
+/**
+ * 生成随机数
+ * @param { number } len: 长度
+ */
+function random(len: number): number{
+  return Math.floor(Math.random() * len);
+}
+
+/* 生成卡组 */
+function kazu(CARD: Object, len: { n: number, r: number, sr: number, ssr: number }): Array{
+  const kz: Array = [];
+
+  // 抽取ssr卡形成卡组
+  for(let i: number = 0, j: number = CARD.SSR.length; i < len.ssr; i++){
+    kz.push({
+      ...CARD.SSR[random(j)],
+      level: 'SSR'
+    });
+  }
+
+  // 抽取sr卡形成卡组
+  for(let i: number = 0, j: number = CARD.SR.length; i < len.sr; i++){
+    kz.push({
+      ...CARD.SR[random(j)],
+      level: 'SR'
+    });
+  }
+
+  // 抽取r卡形成卡组
+  for(let i: number = 0, j: number = CARD.R.length; i < len.r; i++){
+    kz.push({
+      ...CARD.R[random(j)],
+      level: 'R'
+    });
+  }
+
+  // 抽取n卡形成卡组
+  for(let i: number = 0, j: number = CARD.N.length; i < len.n; i++){
+    kz.push({
+      ...CARD.N[random(j)],
+      level: 'N'
+    });
+  }
+
+  // 乱序排列
+  const kz2: Array = [];
+  while(kz.length > 0){
+    const index: number = random(kz.length);
+    kz2.push(kz[index]);
+    kz.splice(index, 1);
+  }
+  return kz2;
+}
+
+/**
+ * 抽卡
+ * 每隔三次重新生成卡组
+ * @param { Object } CARD       : 抽卡的配置卡组
+ * @param { number } choukaMoney: 单次抽卡钱数
+ * @param { number } money      : 钱数
+ * @param { Object } len        : 卡组数量配置
+ */
+function chouka(CARD: Object, choukaMoney: number, money: number, len: { n: number, r: number, sr: number, ssr: number }): Object{
+  // 计算抽卡次数
+  const l_128: number = Math.floor(money / (choukaMoney * 10)); // 128的档次可以抽多少次卡
+  const l_12point8: number = Math.floor(money / choukaMoney);   // 12.8的档次可以抽多少次卡
+  const bout: number = l_12point8 + l_128;                      // 总抽卡数
+  let best: ?Object = null;                                     // 最好的卡
+
+  // 抽卡
+  const result: Array = [];
+  let i: number = 0;
+  let ka: ?Array = null;
+  while(i < bout){
+    if(i % 3 === 0){
+      ka = kazu(CARD, len);
+    }
+    const index: number = random(ka.length);
+    const item: Object = ka[index];
+    // 获取卡的最该等级
+    const L: Object = {
+      N: 0,
+      R: 1,
+      SR: 2,
+      SSR: 3
+    };
+    if(best === null){
+      best = item;
+    }else{
+      if(L[item.level] > L[best.level]){
+        best = item;
+      }
+    }
+    result.push(item);
+    i += 1;
+  }
+
+  return {
+    result,
+    best
+  };
+}
+
+export default chouka;
+
+/**
+ * 补卡
+ * @param { Object } CARD: 抽卡的配置卡组
+ * @param { number } num : 补卡次数
+ * @param { Object } len : 卡组数量配置
+ */
+export function buka(CARD: Object, num: number, len: { n: number, r: number, sr: number, ssr: number }): Array{
+  // 抽卡
+  const result: Array = [];
+  let i: number = 0;
+  let ka: ?Array = null;
+  while(i < num){
+    if(i % 3 === 0){
+      ka = kazu(CARD, len);
+    }
+    const index: number = random(ka.length);
+    const item: Object = ka[index];
+    result.push(item);
+    i += 1;
+  }
+
+  return result;
+}
+
+/**
+ * 取出卡组的id，转换成数组
+ * @param { Array } rawArray
+ */
+export function record(rawArray: Array): Array<string>{
+  const r: Array<string> = [];
+  for(let i: number = 0, j: number = rawArray.length; i < j; i++){
+    const item: Object = rawArray[i];
+    r.push(item.id);
+  }
+  return r;
+}
+
+/**
+ * 将卡组转换成对象，并输出文字
+ * @param { Array } rawArray
+ * @param { string } template: 文字模板
+ */
+export function choukaText(rawArray: Array, template: string): string{
+  const r: Object = {};
+  for(let i: number = 0, j: number = rawArray.length; i < j; i++){
+    const item: Object = rawArray[i];
+    if(item.id in r){
+      r[item.id].len += 1;
+    }else{
+      r[item.id] = {
+        len: 1,
+        name: item.name,
+        level: item.level
+      };
+    }
+  }
+  // 循环r，生成文字
+  let txt: string = '';
+  for(const key: string in r){
+    const item: Object = r[key];
+    txt += '\n' + templateReplace(template, {
+      name: item.name,
+      level: item.level,
+      len: item.len
+    });
+  }
+  return txt;
+}
---
+++ app/src/components/modian/cardsCb.js
@@ -0,0 +1,62 @@
+/* 查询卡牌数量 */
+import $ from 'jquery';
+
+function cardsCb(command: string[], qq: CoolQ): void{
+  const basic: Object = qq.option.basic;
+  // 如果没有昵称，取消查找
+  if(command[1] === undefined){
+    return void 0;
+  }
+  $.ajax({
+    url: basic.choukaSearchUrl,
+    dataType: 'json',
+    type: 'POST',
+    data: {
+      nickname: command[1],
+      token: basic.choukaToken
+    },
+    success(data: Object, status: string, xhr: XMLHttpRequest): void{
+      let txt: string = `【${ command[1] }】的抽卡情况：`;
+      if(data.length === 0){
+        txt += '没有抽卡。';
+      }else{
+        const record: Object = JSON.parse(data[0].record);
+        const card: Object = qq.card;
+        // 获取N卡情况
+        const nArray: [] = [];
+        for(let i: number = 0, j: number = card.N.length; i < j; i++){
+          const item: Object = card.N[i];
+          if(item.id in record) nArray.push(`${ item.name } * ${ record[item.id] }`);
+        }
+        if(nArray.length > 0) txt += `\n【N】:\n${ nArray.join('，') }`;
+        // 获取R卡情况
+        const rArray: [] = [];
+        for(let i: number = 0, j: number = card.R.length; i < j; i++){
+          const item: Object = card.R[i];
+          if(item.id in record) rArray.push(`${ item.name } * ${ record[item.id] }`);
+        }
+        if(rArray.length > 0) txt += `\n【R】:\n${ rArray.join('，') }`;
+        // 获取SR卡情况
+        const srArray: [] = [];
+        for(let i: number = 0, j: number = card.SR.length; i < j; i++){
+          const item: Object = card.SR[i];
+          if(item.id in record) srArray.push(`${ item.name } * ${ record[item.id] }`);
+        }
+        if(srArray.length > 0) txt += `\n【SR】:\n${ srArray.join('，') }`;
+        // 获取SSR卡情况
+        const ssrArray: [] = [];
+        for(let i: number = 0, j: number = card.SSR.length; i < j; i++){
+          const item: Object = card.SSR[i];
+          if(item.id in record) ssrArray.push(`${ item.name } * ${ record[item.id] }`);
+        }
+        if(ssrArray.length > 0) txt += `\n【SSR】:\n${ ssrArray.join('，') }`;
+      }
+      qq.sendMessage(txt);
+    },
+    error(xhr: XMLHttpRequest, status: string, type: string): void{
+      qq.sendMessage(xhr.responseJSON.message);
+    }
+  });
+}
+
+export default cardsCb;
